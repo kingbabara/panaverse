c886f64e8ef8a041ba7360fd32897a1a91a626b6
ABDULLAH <165952645+kingbabara@users.noreply.github.com>
2024-04-05 11:50:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//
+//let num1 ='apples';
+//myBox = 'orange'
+//myBox = "peach"
+//console.log(the super,)
+var num1 = 20;
+var num2 = 10;
+var quotient = num1 / num2;
+var remainder = num1 % num2;
+console.log(num1===num2);
---
+++ main.ts
@@ -0,0 +1,12 @@
+//
+
+//let num1 ='apples';
+//myBox = 'orange'
+//myBox = "peach"
+//console.log(the super,)
+let num1 = 20;
+let num2 = 10;
+let quotient = num1/num2;
+
+let remainder = num1%num2;
+console.log(10%3)
